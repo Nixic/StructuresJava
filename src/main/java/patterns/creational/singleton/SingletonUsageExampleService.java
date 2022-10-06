d5b87c7f1b7fbcb633b0c9926a8f3e2f6c5743bd
Opalev Vyacheslav <VOpalev@IBS.RU>
2022-10-06 21:56:18 +0500

    refactoring

--- src/main/java/patterns/creational/singleton/SingletonUsageExampleService.java
+++ src/main/java/patterns/creational/singleton/SingletonUsageExampleService.java
@@ -2,13 +2,13 @@ package patterns.creational.singleton;
 
 public class SingletonUsageExampleService {
 
-    private static final SingletonClass LAZY_SINGLETON_CLASS = SingletonClass.getInstance();
+    private static final SingletonClass SINGLETON_CLASS = SingletonClass.getInstance();
     private static final SynchronizedAndDoubleCheckedSingleton SYNCHRONIZED_SINGLETON_CLASS = SynchronizedAndDoubleCheckedSingleton.getInstance();
     private static final SingletonHolderClass SINGLETON_HOLDER_CLASS = SingletonHolderClass.getInstance();
 
-    public static SingletonClass getLazySingletonClass() {
-        LAZY_SINGLETON_CLASS.setInfo("New class info of lazy singleton");
-        return LAZY_SINGLETON_CLASS;
+    public static SingletonClass getSingletonClass() {
+        SINGLETON_CLASS.setInfo("New class info of lazy singleton");
+        return SINGLETON_CLASS;
     }
 
     public static SynchronizedAndDoubleCheckedSingleton getSynchronizedSingletonClass() {
@@ -22,8 +22,8 @@ public class SingletonUsageExampleService {
     }
 
     public static void main(String[] args) {
-        System.out.println(LAZY_SINGLETON_CLASS.getInfo());
-        System.out.println(getLazySingletonClass().getInfo());
+        System.out.println(SINGLETON_CLASS.getInfo());
+        System.out.println(getSingletonClass().getInfo());
 
         System.out.println(SYNCHRONIZED_SINGLETON_CLASS.getInfo());
         System.out.println(getSynchronizedSingletonClass().getInfo());
